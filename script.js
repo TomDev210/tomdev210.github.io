e5e5a96d1d0667b5467644550a8a0bb451cd6f49
Rajalakshmi G <165365222+TomDev210@users.noreply.github.com>
2026-03-12 00:28:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+/* SIDEBAR TOGGLE */
+
+function toggleMenu() {
+    const sidebar = document.getElementById("sidebar");
+    sidebar.classList.toggle("active");
+}
+
+
+/* CLOSE SIDEBAR WHEN NAV LINK IS CLICKED */
+
+const sidebarLinks = document.querySelectorAll(".sidebar a");
+
+sidebarLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        document.getElementById("sidebar").classList.remove("active");
+    });
+});
+
+
+/* CLOSE SIDEBAR WHEN CLICKING OUTSIDE */
+
+document.addEventListener("click", function (event) {
+
+    const sidebar = document.getElementById("sidebar");
+    const menuBtn = document.querySelector(".menu-btn");
+
+    if (
+        sidebar.classList.contains("active") &&
+        !sidebar.contains(event.target) &&
+        !menuBtn.contains(event.target)
+    ) {
+        sidebar.classList.remove("active");
+    }
+
+});
+
+
+/* TYPING ANIMATION */
+
+const text = "AI & Data Analytics Developer";
+let index = 0;
+
+function typeEffect() {
+
+    if (index < text.length) {
+        document.getElementById("typing").innerHTML += text.charAt(index);
+        index++;
+        setTimeout(typeEffect, 80);
+    }
+
+}
+
+typeEffect();
+
+
+
+/* SCROLL ANIMATION */
+
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+
+    sections.forEach(sec => {
+
+        const top = sec.getBoundingClientRect().top;
+
+        if (top < window.innerHeight - 100) {
+            sec.classList.add("show");
+        }
+
+    });
+
+});
+
